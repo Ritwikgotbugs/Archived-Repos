1ba21665c77176e517c42890e0d8bc1b5c790e88
Ritwikgotbugs <shritwik04@gmail.com>
2024-08-17 13:03:10 +0530

    fix index.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,19 @@
 'use client';
 
-import { Outfit } from "next/font/google";
 import { useRouter } from "next/navigation";
-const font = Outfit({
-  weight: ["400"],
-  subsets: ["latin"],
+import { useEffect } from "react";
 
-});
-export default function Home() {
+
+
+
+export default function HomeSkeleton() {
   const router = useRouter();
-  
+
+  useEffect(() => {
+    router.push('/about');
+  }, [router]);
   return (
-    <main className="flex h-full flex-col items-center justify-center bg-primary">
-      <div className="space-y-6 text-center">
-        <h1 className="text-6xl text-white font-bold">Auth</h1>
-        <p className="font-medium text-white text-xl">Robust Authentication system</p>
-        <button onClick={()=> router.push('/about')} className="bg-white text-primary text-lg font-medium px-6 py-2 rounded-lg">Get Started</button>
-        <div>
-        </div>
-      </div>
-    </main>
+    <div className="bg-primary h-full w-full"/>
+      
   );
 }
