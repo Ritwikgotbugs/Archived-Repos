5089dc76be8b32b012ae85301ed825a7b6b24f22
bhumika1331 <sharmabhumika54@gmail.com>
2024-10-16 23:08:40 +0530

    feat: Acceptance updated

--- src/app/(stages)/acceptance/page.tsx
+++ src/app/(stages)/acceptance/page.tsx
@@ -1,9 +1,9 @@
 'use client';
 import { Button } from "@/components/ui/button";
-import { db } from "@/firebase/client"; // Make sure to point to your firebase config
+import { db } from "@/firebase/client";
 import { addDoc, collection } from "firebase/firestore";
 import { NextPage } from "next";
-import Link from "next/link"; // Import Next.js Link
+import Link from "next/link";
 import { useState } from "react";
 
     const Home: NextPage = () => {
@@ -38,9 +38,9 @@ import { useState } from "react";
     };
 
     return (
-        <div className="bg-white p-6 max-w-screen mx-40">
-        <h1 className="text-5xl font-bold mb-4 mt-20">Acceptance</h1>
-        <h4 className="text-xl font-bold text-gray-500">
+        <div className="bg-white p-6 max-w-screen mx-72">
+        <h1 className="text-5xl font-semibold mb-4 mt-20">Acceptance</h1>
+        <h4 className="text-xl font-semibold text-gray-500">
             Acceptance – see you through the looking glass, looking only at you
         </h4>
         <p className="italic text-xl text-black mb-4 text-center mt-10">
@@ -51,7 +51,7 @@ import { useState } from "react";
             During "Acceptance", one is open to the reality of the present moment
             without being in a state of belief or disbelief.
         </p>
-        <p className="italic text-gray-600 mb-4 text-xl text-center">
+        <p className="italic text-black mb-4 text-xl text-center">
             "Happiness can only exist in Acceptance" – George Orwell
         </p>
         <p className="mt-10 text-gray-900 mb-4 text-xl font-semibold">
@@ -60,11 +60,12 @@ import { useState } from "react";
         </p>
         <form onSubmit={handleSubmit}>
             {/* Section 1: Practice Self-Kindness */}
-            <h2 className="text-xl font-bold mb-2">1. Practice Self-Kindness</h2>
-            <p className="mb-2">
+            <h2 className="text-xl font-bold mb-2 mt-10 text-black">1. Practice Self-Kindness</h2>
+            <p className="text-black mb-4 font-semibold">Try writing the following about you</p>
+            <p className="mb-2 text-black">
             Imagine the time you were hurt or struggling to finish something and
             feeling down. Show yourself kindness and understanding by writing to
-            yourself a reassuring note.
+            yourself a reassuring note. <span className="text-black text-bold ">(5 lines)</span>
             </p>
             <textarea
             value={selfKindness}
@@ -78,14 +79,20 @@ import { useState } from "react";
             rows={5}
             required
             />
-            <ul className="text-gray-600 text-sm mb-4">
+            <ul className="text-black text-sm mb-6">
+            <li className="text-bold text-black underline font-bold">Rules</li>
             <li>1. Don’t be overly critical or angry</li>
             <li>2. Acknowledge the harm of self-judgment</li>
             <li>3. Write to you with warmth and patience</li>
             </ul>
 
-            <h2 className="text-xl font-bold mb-2">2. Embrace Common Humanity</h2>
-            <p className="mb-2">
+            <h2 className="text-2xl font-bold mb-2 mt-10">2. Embrace Common Humanity</h2>
+            <p className="text-black mb-4 font-semibold">Understand that  suffering and imperfection are universal experiences-</p>
+            <p className="mb-2 text-black">
+            It happens to different people at different time different form and different
+            intensity but is always experience by all at some time or other so 
+            </p>
+            <p className="mb-2 text-black">
             List any 3 struggles you feel others have had the same as you or try
             to find out from friends. “Know that you're not alone in your
             struggles.”
@@ -102,9 +109,12 @@ import { useState } from "react";
             rows={5}
             required
             />
-
-            <h2 className="text-xl font-bold mb-2">3. Cultivate Mindfulness</h2>
-            <p className="mb-2">
+            <p className="mb-2 text-black">
+            This will enable you to build empathy and connection with your fellow humans- most often we don’t 
+            do these or see others pain as were busy battling ours at times its ok so seek compassion to self by relating with others .
+            </p>
+            <h2 className="text-2xl font-bold mb-2 mt-10 text-black">3. Cultivate Mindfulness</h2>
+            <p className="mb-2 text-black font-semibold">
             Stay present in the moment and observe your thoughts and emotions
             without judgment. List ways you can do this. List your favorite
             mindfulness technique.
@@ -124,14 +134,14 @@ import { useState } from "react";
 
             <div className="flex items-center justify-center flex-col">
             <Button 
-    className="bg-teal-900 text-white py-2 rounded-lg flex items-center justify-center hover:bg-teal-700 my-4 px-6 text-lg"
+    className="bg-[#11857D] text-white py-2 rounded-lg flex items-center justify-center hover:bg-teal-700 my-4 px-6 text-lg"
     onClick={handleSubmit} >
     Submit
     </Button>
             <p className="font-light text-slate-500 text-sm mt-3 cursor-pointer border-slate-500">
                 Report a problem?
             </p>
-            <Link href="/" passHref>
+            <Link href="/post-stage" passHref>
                 <Button className="border-2 border-[#11857D] mt-2 bg-white text-[#11857D]">
                 <span className="text-xl text-[#11857D] mx-4">&#8592;</span>Back to
                 Home Page
