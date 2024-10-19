d2c456c6ebd5f565869c17036c19a84872e6b0be
bhumika1331 <sharmabhumika54@gmail.com>
2024-10-19 19:23:36 +0530

    feat : acceptance and let-down page made responsive

--- src/app/(stages)/acceptance/page.tsx
+++ src/app/(stages)/acceptance/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button } from "@/components/ui/button";
+    import { Button } from "@/components/ui/button";
 import { db } from "@/firebase/client";
 import { addDoc, collection } from "firebase/firestore";
 import { NextPage } from "next";
@@ -19,15 +19,15 @@ import { useState } from "react";
         e.preventDefault();
 
         try {
-        // Add a new document to Firestore
+        
         await addDoc(collection(db, "entries"), {
-            selfKindness: selfKindness,
-            commonHumanity: commonHumanity,
-            mindfulness: mindfulness,
+            selfKindness,
+            commonHumanity,
+            mindfulness,
             createdAt: new Date().toISOString(),
         });
 
-        // Clear the input fields after submission
+        
         setSelfKindness("");
         setCommonHumanity("");
         setMindfulness("");
@@ -39,34 +39,31 @@ import { useState } from "react";
     };
 
     return (
-        <div className="bg-white p-6 max-w-screen mx-72">
-        <h1 className="text-5xl font-semibold mb-4 mt-20">Acceptance</h1>
-        <h4 className="text-xl font-semibold text-gray-500">
+        <div className="bg-white p-6 md:px-10 lg:px-24 xl:px-32 max-w-screen-lg mx-auto">
+        <h1 className="text-3xl md:text-5xl font-semibold mb-4 mt-10 text-left">Acceptance</h1>
+        <h4 className="text-lg md:text-xl font-semibold text-gray-500 text-left">
             Acceptance – see you through the looking glass, looking only at you
         </h4>
-        <p className="italic text-xl text-black mb-4 text-center mt-10">
-            “Experiencing events fully and without defense as they are” – Hayes,
-            1994.
+        <p className="italic text-lg md:text-xl text-black mb-4 text-center mt-6 md:mt-10">
+            “Experiencing events fully and without defense as they are” – Hayes, 1994.
         </p>
-        <p className="text-black text-xl mb-4">
-            During "Acceptance", one is open to the reality of the present moment
-            without being in a state of belief or disbelief.
+        <p className="text-black text-base md:text-xl mb-4 text-leftr">
+            During "Acceptance", one is open to the reality of the present moment without being in a state of belief or disbelief.
         </p>
-        <p className="italic text-black mb-4 text-xl text-center">
+        <p className="italic text-black mb-4 text-base md:text-xl text-center">
             "Happiness can only exist in Acceptance" – George Orwell
         </p>
-        <p className="mt-10 text-gray-900 mb-4 text-xl font-semibold">
-            Open your KD – review all you have written one you have done lets try
-            the below tasks
+        <p className="mt-10 text-gray-900 mb-4 text-lg md:text-xl font-semibold">
+            Open your KD – review all you have written one you have done lets try the below tasks
         </p>
+
         <form onSubmit={handleSubmit}>
-            {/* Section 1: Practice Self-Kindness */}
-            <h2 className="text-xl font-bold mb-2 mt-10 text-black">1. Practice Self-Kindness</h2>
-            <p className="text-black mb-4 font-semibold">Try writing the following about you</p>
-            <p className="mb-2 text-black">
-            Imagine the time you were hurt or struggling to finish something and
-            feeling down. Show yourself kindness and understanding by writing to
-            yourself a reassuring note. <span className="text-black text-bold ">(5 lines)</span>
+            
+            <h2 className="text-lg md:text-xl font-bold mb-2 mt-10 text-black">1. Practice Self-Kindness</h2>
+            <p className="text-black mb-2 font-semibold">Try writing the following about you</p>
+            <p className="text-black mb-2">
+            Imagine the time you were hurt or struggling to finish something and feeling down. Show yourself kindness and understanding by writing to
+            yourself a reassuring note. <span className="font-bold">(5 lines)</span>
             </p>
             <textarea
             value={selfKindness}
@@ -81,22 +78,21 @@ import { useState } from "react";
             required
             />
             <ul className="text-black text-sm mb-6">
-            <li className="text-bold text-black underline font-bold">Rules</li>
+            <li className="font-bold underline">Rules</li>
             <li>1. Don’t be overly critical or angry</li>
             <li>2. Acknowledge the harm of self-judgment</li>
             <li>3. Write to you with warmth and patience</li>
             </ul>
 
-            <h2 className="text-2xl font-bold mb-2 mt-10">2. Embrace Common Humanity</h2>
-            <p className="text-black mb-4 font-semibold">Understand that  suffering and imperfection are universal experiences-</p>
-            <p className="mb-2 text-black">
-            It happens to different people at different time different form and different
-            intensity but is always experience by all at some time or other so 
+            <h2 className="text-lg md:text-xl font-bold mb-2 mt-10">2. Embrace Common Humanity</h2>
+            <p className="text-black mb-4 font-semibold">
+            Understand that suffering and imperfection are universal experiences.
             </p>
-            <p className="mb-2 text-black">
-            List any 3 struggles you feel others have had the same as you or try
-            to find out from friends. “Know that you're not alone in your
-            struggles.”
+            <p className="text-black mb-2">
+            It happens to different people at different times, in different forms and intensities but is always experienced by all at some time.
+            </p>
+            <p className="text-black mb-2">
+            List any 3 struggles you feel others have had the same as you or try to find out from friends. “Know that you're not alone in your struggles.”
             </p>
             <textarea
             value={commonHumanity}
@@ -110,16 +106,14 @@ import { useState } from "react";
             rows={5}
             required
             />
-            <p className="mb-2 text-black">
-            This will enable you to build empathy and connection with your fellow humans- most often we don’t 
-            do these or see others pain as were busy battling ours at times its ok so seek compassion to self by relating with others .
+            <p className="text-black mb-2">
+            This will enable you to build empathy and connection with your fellow humans – most often we don’t do this or see others' pain as we are busy
+            battling our own.
             </p>
-            <h2 className="text-2xl font-bold mb-2 mt-10 text-black">3. Cultivate Mindfulness</h2>
-            <p className="mb-2 text-black font-semibold">
-            Stay present in the moment and observe your thoughts and emotions
-            without judgment. List ways you can do this. List your favorite
-            mindfulness technique.
-            </p>
+
+            
+            <h2 className="text-lg md:text-xl font-bold mb-2 mt-10 text-black">3. Cultivate Mindfulness</h2>
+            <p className="text-black mb-2 font-semibold">Stay present in the moment and observe your thoughts and emotions without judgment. List ways you can do this. List your favorite mindfulness technique.</p>
             <textarea
             value={mindfulness}
             onChange={(e) => setMindfulness(e.target.value)}
@@ -134,18 +128,16 @@ import { useState } from "react";
             />
 
             <div className="flex items-center justify-center flex-col">
-            <Button 
-    className="bg-[#11857D] text-white py-2 rounded-lg flex items-center justify-center hover:bg-teal-700 my-4 px-6 text-lg"
-    onClick={handleSubmit} >
-    Submit
-    </Button>
-            <p className="font-light text-slate-500 text-sm mt-3 cursor-pointer border-slate-500">
-                Report a problem?
-            </p>
+            <Button
+                className="bg-[#11857D] text-white py-2 rounded-lg flex items-center justify-center hover:bg-teal-700 my-4 px-6 text-lg"
+                onClick={handleSubmit}
+            >
+                Submit
+            </Button>
+            <p className="font-light text-slate-500 text-sm mt-3 cursor-pointer border-slate-500">Report a problem?</p>
             <Link href="/post-stage" passHref>
                 <Button className="border-2 border-[#11857D] mt-2 bg-white text-[#11857D]">
-                <span className="text-xl text-[#11857D] mx-4">&#8592;</span>Back to
-                Home Page
+                <span className="text-xl text-[#11857D] mx-4">&#8592;</span>Back to Home Page
                 </Button>
             </Link>
             </div>
--- src/app/(stages)/let-downs/page.tsx
+++ src/app/(stages)/let-downs/page.tsx
@@ -1,9 +1,77 @@
- 'use client';
+//  'use client';
+// import WordSelection from '@/components/global/WordSelection';
+// import { Button } from '@/components/ui/button';
+
+// export default function Home() {
+//   const wordsList1 = [
+//     'Pessimistic', 'Depressed', 'Reduced Effort', 'Gloomy', 'Doubtful',
+//     'Confident', 'Bright', 'Encouraging', 'Lead', 'Motivation', 'Driven',
+//     'Reassuring', 'Lethargy', 'Optimistic', 'Prompt'
+//   ];
+
+//   const wordsList2 = [
+//     'Escape', 'Resistance', 'Defiance', 'Problem Solving', 'Helpless',
+//     'Vulnerable', 'Sensitive', 'Feeble', 'Opposition', 'Perceptive',
+//     'Reframing Your Thoughts', 'Convincing Support Seeking'
+//   ];
+
+//   return (
+//     <div className="min-h-screen py-10 px-4 bg-white mx-40">
+//       {/* Main header section */}
+//       <div className="p-6 mb-8  bg-white text-justify">
+//         <h1 className="text-5xl font-bold text-black mb-2">Failure</h1>
+//         <p className="text-3xl font-semibold text-gray-600 ">
+//           The let downs - gives us step to stand on
+//         </p>
+//         <p className="text-black ">
+//           Failure is defined as the gap between what is expected or desired and what is actually experienced – Henry et al 2019.
+//         </p>
+//         <p className="text-black mb-4">
+//           So today we are learning on the go, so let’s learn a few definitions:
+//         </p>
+        
+//         <div className="space-y-4">
+//           <p className="font-bold">Based on mindset theory:</p>
+          
+//           <p className="text-black">
+//             Fixed Mindset- Individuals have a fixed mindset—they believe that intelligence and capacity for specific abilities are unchangeable traits. (Dweck, 2000, 2006)
+
+
+
+//             Growth Mindset - Those with a growth mindset believe that these qualities are malleable and that the brain and our abilities can grow over time and through effort. (Dweck, 2000, 2006)
+
+
+//             Performance - Individuals with a performance goal orientation, appearing competent to those around them is the central motivating factor. (Henry et al 2019)
+          
+
+          
+//             Mastery - Individuals with mastery goal orientations are motivated by a desire to achieve competence in a task. (Henry et al 2019)
+//           </p>
+//         </div>
+
+//         <p className="text-black mt-6">
+//           Let’s start the journey of self-discovery. The following is adapted from: Henry, M. A., Shorter, S., Charkoudian, L., Heemstra, J. M., & Corwin, L. A. (2019). FALL is not a four-letter word: A theoretical framework for exploring undergraduate students’ approaches to academic challenge and responses to failure in STEM learning environments. CBE—Life Sciences Education, 18(1), ar11.
+//         </p>
+//       </div>
+
+//       {/* Word selection with multiple lists */}
+//       <WordSelection
+//         lists={[wordsList1, wordsList2]}  
+//         maxSelection={9}                 
+//         userName="defaultUser"            
+//       />
+//       <div className='flex items-center justify-center flex-col'>
+// <Button  className="bg-teal-900 text-white py-2 rounded-lg flex items-center justify-center hover:bg-teal-700 my-4 px-6 text-lg"> Submit</Button>
+// <p className='font-light text-slate-500 text-sm mt-3 cursor-pointer'>Report a problem?</p>
+
+// </div>
+//     </div>
+//   );
+// }
+
+'use client';
 import WordSelection from '@/components/global/WordSelection';
 import { Button } from '@/components/ui/button';
-import { useRouter } from 'next/navigation';
-import React from 'react';
-import { IoIosArrowRoundBack } from 'react-icons/io';
 
 export default function Home() {
   const wordsList1 = [
@@ -19,55 +87,57 @@ export default function Home() {
   ];
 
   return (
-    <div className="min-h-screen py-10 px-4 bg-white mx-40">
-      {/* Main header section */}
-      <div className="p-6 mb-8  bg-white text-justify">
-        <h1 className="text-5xl font-bold text-black mb-2">Failure</h1>
-        <p className="text-3xl font-semibold text-gray-600 ">
+    <div className="min-h-screen py-10 px-4 sm:px-6 md:px-10 bg-white max-w-screen-lg mx-auto">
+      
+      <div className="p-6 mb-8 bg-white text-justify">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-black mb-4">Failure</h1>
+        <p className="text-xl sm:text-2xl md:text-3xl font-semibold text-gray-600 mb-4">
           The let downs - gives us step to stand on
         </p>
-        <p className="text-black ">
+        <p className="text-base sm:text-lg md:text-xl text-black mb-4">
           Failure is defined as the gap between what is expected or desired and what is actually experienced – Henry et al 2019.
         </p>
-        <p className="text-black mb-4">
+        <p className="text-base sm:text-lg md:text-xl text-black mb-4">
           So today we are learning on the go, so let’s learn a few definitions:
         </p>
-        
+
         <div className="space-y-4">
-          <p className="font-bold">Based on mindset theory:</p>
-          
-          <p className="text-black">
-            Fixed Mindset- Individuals have a fixed mindset—they believe that intelligence and capacity for specific abilities are unchangeable traits. (Dweck, 2000, 2006)
-
-
-
+          <p className="font-bold text-base sm:text-lg md:text-xl">Based on mindset theory:</p>
+          <p className="text-black text-base sm:text-lg md:text-xl">
+            Fixed Mindset - Individuals have a fixed mindset—they believe that intelligence and capacity for specific abilities are unchangeable traits. (Dweck, 2000, 2006)
+          </p>
+          <p className="text-black text-base sm:text-lg md:text-xl">
             Growth Mindset - Those with a growth mindset believe that these qualities are malleable and that the brain and our abilities can grow over time and through effort. (Dweck, 2000, 2006)
-
-
+          </p>
+          <p className="text-black text-base sm:text-lg md:text-xl">
             Performance - Individuals with a performance goal orientation, appearing competent to those around them is the central motivating factor. (Henry et al 2019)
-          
-
-          
+          </p>
+          <p className="text-black text-base sm:text-lg md:text-xl">
             Mastery - Individuals with mastery goal orientations are motivated by a desire to achieve competence in a task. (Henry et al 2019)
           </p>
         </div>
 
-        <p className="text-black mt-6">
+        <p className="text-black text-base sm:text-lg md:text-xl mt-6">
           Let’s start the journey of self-discovery. The following is adapted from: Henry, M. A., Shorter, S., Charkoudian, L., Heemstra, J. M., & Corwin, L. A. (2019). FALL is not a four-letter word: A theoretical framework for exploring undergraduate students’ approaches to academic challenge and responses to failure in STEM learning environments. CBE—Life Sciences Education, 18(1), ar11.
         </p>
       </div>
 
-      {/* Word selection with multiple lists */}
+      
       <WordSelection
-        lists={[wordsList1, wordsList2]}  
-        maxSelection={9}                 
-        userName="defaultUser"            
+        lists={[wordsList1, wordsList2]}
+        maxSelection={9}
+        userName="defaultUser"
       />
-      <div className='flex items-center justify-center flex-col'>
-<Button  className="bg-teal-900 text-white py-2 rounded-lg flex items-center justify-center hover:bg-teal-700 my-4 px-6 text-lg"> Submit</Button>
-<p className='font-light text-slate-500 text-sm mt-3 cursor-pointer'>Report a problem?</p>
 
-</div>
+      
+      <div className="flex items-center justify-center flex-col mt-8">
+        <Button className="bg-teal-900 text-white py-2 rounded-lg flex items-center justify-center hover:bg-teal-700 my-4 px-6 text-lg w-full sm:w-auto">
+          Submit
+        </Button>
+        <p className="font-light text-slate-500 text-sm mt-3 cursor-pointer">
+          Report a problem?
+        </p>
+      </div>
     </div>
   );
 }
